1719c8b32ee64963a5995daba5f132c7eb9cb4c1
euler <mombergereuler@gmail.com>
2026-07-02 22:16:29 -0300

    💄 feat: Configurando estilos

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,15 @@
+import { Inter } from 'next/font/google';
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['400', '500'],
+  variable: '--font-inter',
+});
+
+export function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className={`${inter.variable} relative flex min-h-screen`}>
+      {children}
+    </div>
+  );
+}
