43ea898ac1e9fa29b0ca085ca83e85a7879a0252
euler <mombergereuler@gmail.com>
2026-07-07 21:02:18 -0300

    ✨ feat: Componente de main

---
+++ src/components/MainPage/index.tsx
@@ -0,0 +1,14 @@
+export function MainPage() {
+  return (
+    <main
+      className="
+        flex justify-center items-center gap-6
+        p-6 
+        h-screen w-screen
+        bg-[url('/assets/bg-mobile-light.jpg')] md:bg-[url('/assets/bg-desktop-light.jpg')] bg-cover
+      "
+    >
+
+    </main>
+  );
+}
