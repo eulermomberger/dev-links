ad54f1030bee3000df8d737d721479228a5885f6
euler <mombergereuler@gmail.com>
2026-07-09 22:13:41 -0300

    💄 fix: Corrigindo estilo do avatar

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -8,6 +8,7 @@ export function Avatar({ avatarUrl }: AvatarProps) {
   return (
     <Image
       src={avatarUrl}
+      className="rounded-full w-28 h-28 object-cover ring-2 ring-border"
       alt="Avatar"
       width={112}
       height={112}
